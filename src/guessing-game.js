538c4dcbf6c2f945cd5445c0f4d08f8513ff136e
IliasovDamir <ilyasovdh@gmail.com>
2022-08-10 19:14:38 +0300

    feat: add solution guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,22 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        let result
+        result = Math.round((this.min + this.max) / 2)
+        return result
     }
 
     lower() {
-
+        this.max = Math.round((this.min + this.max) / 2)
     }
 
     greater() {
-
+        this.min = Math.round((this.min + this.max) / 2)
     }
 }
 
